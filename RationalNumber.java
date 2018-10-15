1a8dbc61ab5798620d7e270c632a0a6acc83388d
Elias Ferguson <eferguson00@stuy.edu>
2018-10-15 14:42:56 -0400

    added empty method names

---
+++ RationalNumber.java
@@ -0,0 +1,18 @@
+public class RationalNumber extends RealNumber {
+  private int x;
+  public RationalNumber() {
+    super();
+  }
+  public RationalNumber(int num) {
+    super(num);
+  }
+  public void changeLCD() {
+    //code
+  }
+  public void reduce() {
+    //code
+  }
+  public void cleanUp() {
+    //code that checks for 0 in numerator/1 in denominator.
+  }
+}
